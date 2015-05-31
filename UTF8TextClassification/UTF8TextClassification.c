0387fc0b5fbb8ee8281c76072761ad43155395d7
SLAPaper <slapaper.pku@gmail.com>
2015-05-31 21:00:32 +0800

    doing with load and output

--- UTF8TextClassification/UTF8TextClassification.c
+++ UTF8TextClassification/UTF8TextClassification.c
@@ -1,1 +1,14 @@
 #include "UTF8TextClassification.h"
+
+void load_texts(const struct text_list * l, FILE * in) {
+	if (l != NULL && in != NULL) {
+		uchar * s[BUF_SIZE] = {};
+		while (fgets(s, BUF_SIZE, in) != EOF) {
+			
+		}
+	}
+}
+
+void output_texts(const struct text_list * l, FILE * out) {
+	
+}
--- UTF8TextClassification/UTF8TextClassification.h
+++ UTF8TextClassification/UTF8TextClassification.h
@@ -1,9 +1,17 @@
 #include "ucharLib.h"
 
 #define TYPE_COUNT 4
+#define BUF_SIZE BUFSIZ
 
-struct text
-{
+struct text {
 	struct ustring text;
-	bool types[TYPE_COUNT];
+	int8_t types[TYPE_COUNT];
 };
+
+struct text_list {
+	struct text * list;
+	size_t len;
+};
+
+void load_texts(const struct text_list * l, FILE * in);
+void output_texts(const struct text_list * l, FILE * out);
