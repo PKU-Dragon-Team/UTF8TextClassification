624deff0cb1cbba1be57d27a735ac7964e22d722
SLAPaper <slapaper.pku@gmail.com>
2015-06-08 12:06:36 +0800

    add vector calculation

--- UTF8TextClassification/UTF8TextClassification.c
+++ UTF8TextClassification/UTF8TextClassification.c
@@ -1,5 +1,18 @@
 #include "UTF8TextClassification.h"
 
+static void insert_usa_list(struct ustring_analysis * ap_usa[], struct ustring_analysis * p_usa, size_t hashcode) {
+	if (ap_usa[hashcode] == NULL) {
+		ap_usa[hashcode] = p_usa;
+	}
+	else {
+		struct ustring_analysis * p = ap_usa[hashcode];
+		while (p->next != NULL) {
+			p = p->next;
+		}
+		p->next = p_usa;
+	}
+}
+
 int init_text(struct text ** pp_text, struct ustring * us, int8_t types[TYPE_COUNT]) {
 	if (pp_text == NULL) {
 		return -1;
@@ -108,7 +121,7 @@ int load_texts(FILE * in, struct text_list * p_tlist) {
 			struct text * t = NULL;
 			init_text(&t, us, types);
 
-			if (i + 1 > p_tlist->len) {	// Dynamic Expand
+			if (i >= p_tlist->len) {	// Dynamic Expand
 				if (resize_text_list(p_tlist, p_tlist->len * 2 + 1) != 0) {
 					return -1;
 				}
@@ -178,7 +191,7 @@ int output_char_analysis(FILE * out, const struct uchar_analysis * uca) {
 	}
 
 	fprintf_s(out, "Total Characters: %d\n", uca->total_count);
-	for (int i = 0; i < 130000; ++i) {
+	for (int i = 0; i < MAX_UNICODE; ++i) {
 		if (uca->uchar_list[i] != 0) {
 			fprintf_s(out, "0x%02X\t%5d\n", i, uca->uchar_list[i]);
 		}
@@ -186,54 +199,72 @@ int output_char_analysis(FILE * out, const struct uchar_analysis * uca) {
 	return 0;
 }
 
-int init_hashmap_ustring_analysis(struct hashmap_ustring_analysis ** pp_husa) {
-	if (pp_husa == NULL) {
+int init_hash_vector(struct hash_vector ** pp_hv) {
+	if (pp_hv == NULL) {
 		return -1;
 	}
 
-	*pp_husa = malloc(sizeof(struct hashmap_ustring_analysis));
-	if (*pp_husa == NULL) {
+	*pp_hv = malloc(sizeof(struct hash_vector));
+	if (*pp_hv == NULL) {
 		return -1;
 	}
 
-	(*pp_husa)->usa_list = calloc(BASE_HASH_LEN, sizeof(struct ustring_analysis *));
-	if ((*pp_husa)->usa_list == NULL) {
+	(*pp_hv)->usa_list = calloc(BASE_HASH_LEN, sizeof(struct ustring_analysis *));
+	if ((*pp_hv)->usa_list == NULL) {
 		return -1;
 	}
-	(*pp_husa)->hashlen = BASE_HASH_LEN;
-	(*pp_husa)->total_count = 0;
-	(*pp_husa)->count = 0;
+	(*pp_hv)->hashlen = BASE_HASH_LEN;
+	(*pp_hv)->total_count = 0;
+	(*pp_hv)->count = 0;
 
 	return 0;
 }
 
-int rehash_hashmap_ustring_analysis(struct hashmap_ustring_analysis * p_husa, size_t hashlen) {
-	if (p_husa == NULL) {
+int rehash_hash_vector(struct hash_vector * p_hv, size_t hashlen) {
+	if (p_hv == NULL) {
 		return -1;
 	}
+
+	struct ustring_analysis ** temp = calloc(hashlen, sizeof(struct ustring_analysis *));
+	if (temp == NULL) {
+		return -1;
+	}
+
+	for (size_t i = 0; i < p_hv->hashlen; ++i) {
+		struct ustring_analysis * p = p_hv->usa_list[i];
+		while (p != NULL) {
+			insert_usa_list(temp, p, hash_ustring(p->us, HASH_SEED, hashlen));
+			struct ustring_analysis * q = p->next;
+			p->next = NULL;
+			p = q;
+		}
+	}
+	free(p_hv->usa_list);
+	p_hv->usa_list = temp;
+	p_hv->hashlen = hashlen;
 	return 0;
 }
 
-int append_hashmap_ustring_analysis(struct hashmap_ustring_analysis * p_husa, const struct ustring * cp_us, parser f) {
-	if (p_husa == NULL || cp_us == NULL || f == NULL) {
+int append_hash_vector(struct hash_vector * p_hv, const struct ustring * cp_us, parser f) {
+	if (p_hv == NULL || cp_us == NULL || f == NULL) {
 		return -1;
 	}
 
 	p_uspl parse_list = f(cp_us);
 
-	for (size_t i = 0; i < parse_list->len - 1; ++i) {
+	for (size_t i = 0; i + 1 < parse_list->len; ++i) {
 		// check if hashmap is overload
-		if (p_husa->count * 2 > p_husa->hashlen) {
-			rehash_hashmap_ustring_analysis(p_husa, p_husa->hashlen * 2 + 1);
+		if (p_hv->count * 2 > p_hv->hashlen) {
+			rehash_hash_vector(p_hv, p_hv->hashlen * 2 + 1);
 		}
 
 		struct ustring * temp = malloc(sizeof(struct ustring));
 		init_ustring(&temp, index, NULL, 0);
 		slice_ustring(cp_us, temp, parse_list->parse_list[i], parse_list->parse_list[i + 1]);
 
-		size_t hashcode = hash_ustring(temp, 0, p_husa->hashlen);
+		size_t hashcode = hash_ustring(temp, HASH_SEED, p_hv->hashlen);
 		// search for existence
-		struct ustring_analysis * p = p_husa->usa_list[hashcode];
+		struct ustring_analysis * p = p_hv->usa_list[hashcode];
 		if (p != NULL) {
 			while (p->next != NULL) {
 				if (compare_ustring(p->next->us, temp) == 0) {
@@ -263,28 +294,79 @@ int append_hashmap_ustring_analysis(struct hashmap_ustring_analysis * p_husa, co
 			p_ua->us = temp;
 			p_ua->count = 1;
 			p_ua->next = NULL;
-			p_husa->usa_list[hashcode] = p_ua;
-			++p_husa->count;
+			p_hv->usa_list[hashcode] = p_ua;
+			++p_hv->count;
 		}
-		++p_husa->total_count;
+		++p_hv->total_count;
 	}
 	return 0;
 }
 
-int clear_hashmap_ustring_analysis(struct hashmap_ustring_analysis ** pp_husa) {
-	if (pp_husa == NULL || *pp_husa == NULL || (*pp_husa)->usa_list == NULL) {
+int add_hash_vector(struct hash_vector * p_hv1, const struct hash_vector * p_hv2) {
+	if (p_hv1 == NULL || p_hv2 == NULL) {
 		return -1;
 	}
 
-	for (size_t i = 0; i < (*pp_husa)->hashlen; ++i) {
-		struct ustring_analysis * p = (*pp_husa)->usa_list[i];
+	return 0;
+}
+
+int sub_hash_vector(struct hash_vector * p_hv1, const struct hash_vector * p_hv2) {
+	if (p_hv1 == NULL || p_hv2 == NULL) {
+		return -1;
+	}
+	return 0;
+}
+
+long long product_hash_vector(const struct hash_vector * p_hv1, const struct hash_vector * p_hv2) {
+	long long product = 0;
+
+	for (size_t i = 0; i < p_hv1->hashlen; ++i) {
+		struct ustring_analysis * p = p_hv1->usa_list[i];
+		struct ustring_analysis * q;
+		if (p_hv1->hashlen == p_hv2->hashlen) {
+			q = p_hv2->usa_list[i];
+		}
+		else {
+			q = p_hv2->usa_list[hash_ustring(p->us, HASH_SEED, p_hv2->hashlen)];
+		}
+
+		while (p != NULL) {
+			while (q != NULL) {
+				if (compare_ustring(p->us, q->us) == 0) {
+					product += p->count * q->count;
+				}
+				q = q->next;
+			}
+			p = p->next;
+		}
+	}
+	return product;
+}
+
+unsigned long long len2_hash_vector(const struct hash_vector * p_hv) {
+	long long len2 = 0;
+
+	for (size_t i = 0; i < p_hv->hashlen; ++i) {
+		struct ustring_analysis * p = p_hv->usa_list[i];
+
+	}
+	return len2;
+}
+
+int clear_hash_vector(struct hash_vector ** pp_hv) {
+	if (pp_hv == NULL || *pp_hv == NULL || (*pp_hv)->usa_list == NULL) {
+		return -1;
+	}
+
+	for (size_t i = 0; i < (*pp_hv)->hashlen; ++i) {
+		struct ustring_analysis * p = (*pp_hv)->usa_list[i];
 		while (p != NULL) {
 			clear_ustring(&p->us);
 			p = p->next;
 		}
 	}
-	free((*pp_husa)->usa_list);
-	free(*pp_husa);
+	free((*pp_hv)->usa_list);
+	free(*pp_hv);
 	return 0;
 }
 
--- UTF8TextClassification/UTF8TextClassification.h
+++ UTF8TextClassification/UTF8TextClassification.h
@@ -7,13 +7,14 @@
 #define BUF_SIZE 10000
 #define BASE_HASH_LEN 65535
 #define MAX_UNICODE 130000
+#define HASH_SEED 0
 
 typedef struct ustring_parse_list * p_uspl;
 typedef p_uspl(*parser)(const struct ustring * cp_us);
 
 struct text {
 	struct ustring * us;
-	struct hashmap_ustring_analysis * usa;
+	struct hash_vector * usa;
 	int8_t types[TYPE_COUNT];
 };
 
@@ -33,8 +34,8 @@ struct ustring_analysis {
 	struct ustring_analysis * next;
 };
 
-struct hashmap_ustring_analysis {
-	long long total_count;
+struct hash_vector {
+	unsigned long long total_count;
 	struct ustring_analysis ** usa_list;
 	size_t hashlen;
 	size_t count;
@@ -44,6 +45,9 @@ struct ustring_parse_list {
 	size_t * parse_list;
 	size_t len;
 };
+
+// Incert p_usa to the hashcode position of ap_usa
+static void insert_usa_list(struct ustring_analysis * ap_usa[], struct ustring_analysis * p_usa, size_t hashcode);
 
 int init_text(struct text ** pp_text, struct ustring * us, int8_t types[TYPE_COUNT]);
 int clear_text(struct text ** pp_text);
@@ -59,10 +63,15 @@ int output_texts(FILE * out, const struct text_list * p_tlist);
 int get_char_analysis(const struct text_list * cp_tlist, struct uchar_analysis * uca);
 int output_char_analysis(FILE * out, const struct uchar_analysis * uca);
 
-int init_hashmap_ustring_analysis(struct hashmap_ustring_analysis ** pp_husa);
-int rehash_hashmap_ustring_analysis(struct hashmap_ustring_analysis * p_husa, size_t hashlen);
-int append_hashmap_ustring_analysis(struct hashmap_ustring_analysis * p_husa, const struct ustring * cp_us, parser f);
-int clear_hashmap_ustring_analysis(struct hashmap_ustring_analysis ** pp_husa);
+int init_hash_vector(struct hash_vector ** pp_hv);
+int rehash_hash_vector(struct hash_vector * p_hv, size_t hashlen);
+int append_hash_vector(struct hash_vector * p_hv, const struct ustring * cp_us, parser f);
+int add_hash_vector(struct hash_vector * p_hv1, const struct hash_vector * p_hv2);
+int sub_hash_vector(struct hash_vector * p_hv1, const struct hash_vector * p_hv2);
+long long product_hash_vector(const struct hash_vector * p_hv1, const struct hash_vector * p_hv2);
+unsigned long long len2_hash_vector(const struct hash_vector * p_hv);
+int clear_hash_vector(struct hash_vector ** pp_hv);
+
 p_uspl blankParser(const struct ustring * cp_us);
 p_uspl ucharParser(const struct ustring * cp_us);
 
