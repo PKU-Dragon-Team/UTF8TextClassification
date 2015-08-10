419aa23db3a2caed473b3c2a2a02137f049f1a1d
SLAPaper <slapaper.pku@gmail.com>
2015-08-10 18:11:39 +0800

    give the detailed output of tester

--- UTF8TextClassification/TrainersAndClassifiers.h
+++ UTF8TextClassification/TrainersAndClassifiers.h
@@ -4,16 +4,21 @@
 #include "UStringVector.h"
 #include "TextUtilities.h"
 
-// The trainer which simply add the vector when the class meets and sub the vector when not
+// Trainer which simply add the vector when the class meets and sub the vector when not
 int naive_trainer(struct hash_vector * ap_hv[TYPE_COUNT + 1], const struct text_list * p_tl, Parser parser, Checker checker);
 
-// The tester using K-Nearest Neighbors algorithm
+// Tester using K-Nearest Neighbors algorithm
 int KNN_tester(FILE * out, struct text_list * p_tl, struct hash_vector * const statistic[TYPE_COUNT + 1], Parser parser, Checker checker);
 
-// The classifier using K-Nearest Neighbors algorithm
+// Classifier using K-Nearest Neighbors algorithm
 int KNN_classifier(FILE * out, struct text_list * p_tl, struct hash_vector * const statistic[TYPE_COUNT + 1], Parser parser, Checker checker);
 
-// Trainer that add the vector
+// Trainer that add the vector (It's a surprise that the Naive Bayes only needs the vector of all the classes)
 int NaiveBayes_trainer(struct hash_vector * ap_hv[TYPE_COUNT + 1], const struct text_list * p_tl, Parser parser, Checker checker);
 
+//
+int NB_tester(FILE * out, struct text_list * p_tl, struct hash_vector * const statistic[TYPE_COUNT + 1], Parser parser, Checker checker);
+
+// 
+int NB_classifier(FILE * out, struct text_list * p_tl, struct hash_vector * const statistic[TYPE_COUNT + 1], Parser parser, Checker checker);
 #endif
